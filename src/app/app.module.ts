bbccb0358c60e1a53f05aaabd18220aee84fe1fe
Mushahid Khan <alikhanchd786@gmail.com>
2020-12-24 23:32:22 +0530

    dashboard component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './pages/movies-list/movies-list.component';
+import { TopratedMoviesListComponent } from './pages/toprated-movies-list/toprated-movies-list.component';
+import { UpcomingMoviesListComponent } from './pages/upcoming-movies-list/upcoming-movies-list.component';
+import { DashboardsComponent } from './components/dashboards/dashboards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    TopratedMoviesListComponent,
+    UpcomingMoviesListComponent,
+    DashboardsComponent
   ],
   imports: [
     BrowserModule,
